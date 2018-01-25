bd0d896c3108dd1335e0613693ace0128c68e45c
mohitgoel188 <35655238+mohitgoel188@users.noreply.github.com>
2018-01-26 00:55:36 +0530

    Update 101
    Added dictionary support.
    updated read me

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AddAll
-A function to add everything possible in passed argument
+A function to add everything possible passed as the argument.
+
+Well, almost anything :-)
+To be specific, the add all function can process following arguments:-
+- integers
+- strings
+- floating values
+- lists 
+- dictionaries
+- other functions
--- addall.py
+++ addall.py
@@ -1,12 +1,16 @@
 import re
+
 def strnum(string):
+'''Function to extract int and float from strings'''
     lst=re.findall('[0-9]+[0-9.]*',string)
     total=0.0
     if len(lst)>0:
         for num in lst:
             total+=float(num)
     return total    
+
 def add(*args):
+'''The add all function'''
     total=0
     for item in args:
         if item is None:
@@ -14,12 +18,16 @@ def add(*args):
         elif type(item) is str:
             total+=strnum(item)
         elif type(item) is list:
-            total+=add(*item)          # *list to unpack list items
+            total+=add(*item)         # *list to unpack list items
+        elif type(item) is dict:
+            for key in item.keys():
+                total+=add(item[key])
         else:
             total+=item;
     return total
-def main():                         
-    print(add(1,2,"got 40 and30 al50.1so",4.0,5.9,[1,2,"make",[10,20,"jhone"]]))
 
-if __name__ == '__main__':          # To use this as a module
+def main():                           # Test Code
+    print(add({'a':8,'b':[2,4,'Roll No. 21']}))
+
+if __name__ == '__main__':            # To use this as a module
     main()
